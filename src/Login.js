47d9550ff3f4dee1cc4eda5e5ff8417075e442ae
Volodymyr Pasternak <vova.paster04@gmail.com>
2018-04-18 17:35:05 +0300

    refactor skeleton, add navbar

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+ 
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h2>LOGIN FORM</h2>
+        <button>click me</button>
+      </div>
+    );
+  }
+}
+ 
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
-import store, { history } from './store';
-import App from './containers/app';
-
-import 'sanitize.css/sanitize.css';
-import './index.css';
-
-const target = document.querySelector('#root');
-
-render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <div>
-        <App />
-      </div>
-    </ConnectedRouter>
-  </Provider>,
-  target
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+import "./index.css";
+ 
+ReactDOM.render(
+  <Main/>, 
+  document.getElementById("root")
 );
