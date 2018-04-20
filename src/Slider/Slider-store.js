027ad4652c0aed804a8e4b93cdc70b0dce9d4b11
DenisPolyarush <92azov92@gmail.com>
2018-04-20 20:00:11 +0300

    Slider

---
+++ src/Slider/Slider-store.js
@@ -0,0 +1,17 @@
+const SliderStore = [
+	{
+		name : "Кролик Петрик",
+		url : 'https://kino-teatr.ua/public/main/films/trailer_12989.jpg'
+	},
+	{
+		name : "Кікбоксер : Реванш",
+		url : 'https://pointspace.com.ua/wp-content/uploads/2018/04/kikbokser.jpg'
+	},
+	{
+		name : "Жага смерті",
+		url : 'https://img.hdo-cdn.ru/2018/03/31/cover/f4b60edbf113bc59788a87e883769ba1-death-wish.jpg'
+	}
+
+]
+
+export default SliderStore;
