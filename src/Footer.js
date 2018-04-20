1368223d824f301aacbce4023ad422e3c9b8dd90
BohdanShuliaka <bohdan.shuliaka@gmail.com>
2018-04-20 19:44:20 +0300

    Navbar

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+   
+class Footer extends Component {
+  render() {
+    return (
+        <footer className="footer bg-dark">
+            
+            <div className="container center">
+            <div className="row justify-content-center">
+                <a className="nav-link" href="/">POPCORN STUDIO &copy; 2018</a>
+            </div>
+            </div>
+        </footer>
+    );
+  }
+}
+ 
+export default Footer;
---
+++ src/Navbar.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import {
+    Route,
+    NavLink,
+    HashRouter
+  } from "react-router-dom"; 
+
+
+class Navbar extends Component {
+   
+  render() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a className="navbar-brand" href="/">LOGO</a>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item">
+            <NavLink exact to="/">Home</NavLink>
+            </li>
+            <li className="nav-item">
+            <NavLink to="/stuff">Stuff</NavLink>
+            </li>
+            <li className="nav-item">
+            <NavLink to="/contact">Contact</NavLink>
+            </li>
+            <li className="nav-item">
+            <NavLink to="/login">Login</NavLink>
+            </li>
+          </ul>
+          
+        </div>
+      </nav>
+
+    );
+  }
+}
+export default Navbar;
+
+
+
+
+
