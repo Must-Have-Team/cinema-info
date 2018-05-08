ed5fc3af4247692dbf1c206fde9240a25179cbc0
Volodymyr Pasternak <vova.paster04@gmail.com>
2018-05-08 17:39:27 +0300

    change path to popper.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./Main";
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-// import "./index.css";
-// import '../node_modules/popper.js/dist/popper.min.js';
-// import '../node_modules/jquery/dist/jquery.min.js';
-// import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-// import '../node_modules/font-awesome/css/font-awesome.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import "./index.css";
+import '../node_modules/popper.js/dist/umd/popper.js';
+import '../node_modules/jquery/dist/jquery.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
+import '../node_modules/font-awesome/css/font-awesome.css';
  
 ReactDOM.render(
   <Main/>,
