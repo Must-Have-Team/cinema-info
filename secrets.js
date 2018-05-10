d0ce362ba055fda0f6f423fae06f1b499ae1eae6
Volodymyr Pasternak <vova.paster04@gmail.com>
2018-05-10 17:06:05 +0300

    remove server

--- secrets.js
+++
@@ -1,9 +0,0 @@
-const secrets = {
-  'db_uri': `mongodb://${process.env.DB_USER_NAME}:${process.env.DB_PASS}@ds163119.mlab.com:63119/${process.env.DB_NAME}`,
-};
-
-module.exports = {
-  requestSecret: function(s) {
-    return secrets[s];
-  },
-};
