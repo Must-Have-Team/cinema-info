149506a462ccce4263c97bf6246470e9162ad66e
Volodymyr Pasternak <vova.paster04@gmail.com>
2018-05-11 18:08:44 +0300

    add schema for sessions

---
+++ model/sessions.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var SessionSchema = new Schema({
+  "id": Number,
+  "begin": String,
+  "end": String,
+  "film_id": Number,
+  "hall_id": Number,
+  "times": [{
+    "id": Number,
+    "time": String,
+    "prices": String,
+    "3d": Boolean,
+    "purchase_allowed": Boolean
+  }]
+});
+
+module.exports = mongoose.model('Session', SessionSchema);
