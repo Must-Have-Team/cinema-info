7134e4d5489cb2668cb833c1cbdb397e6ce9ff6b
DenisPolyarush <92azov92@gmail.com>
2018-05-12 15:09:11 +0300

    delete duplicate search-form

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
 import Slider from './Slider/Slider'
-import SearchForm from './SearchForm/SearchForm';
 import Movies from './Allmovies/AllMovies'
-import FilmStore from './Allmovies/Film-store'
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <SearchForm items={FilmStore}/>
         <Slider />
         <Movies />
       </div>
