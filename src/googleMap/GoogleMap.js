8d88843716a3b3f0910d5582d07d1e97bd234b86
Volodymyr Pasternak <vova.paster04@gmail.com>
2018-05-16 17:12:08 +0300

    debuging some styles

--- src/googleMap/GoogleMap.js
+++ src/googleMap/GoogleMap.js
@@ -8,7 +8,7 @@ const MyMapComponent = compose(
     withProps({
       googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAcSy9SCCQoXnJQhMBPsbjKuu9afVxC8Vs&v=3.exp&libraries=geometry,drawing,places",
       loadingElement: <div style={{ height: `100%` }} />,
-      containerElement: <div style={{ height: `300px`, width: `800px` }} />,
+      containerElement: <div style={{ height: `300px`, width: `auto` }} />,
       mapElement: <div style={{ height: `100%` }} />,
     }),
     withScriptjs,
