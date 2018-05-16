22b772a120798233b2e15006bac388d8d0156b36
DenisPolyarush <92azov92@gmail.com>
2018-05-16 21:16:34 +0300

    change slideImages

--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -61,7 +61,7 @@ class Slider extends Component {
     return (<Link
         className={
         index === this.state.current ? 'slide-item active-slide' : 'slide-item'}
-        key={item.name}
+        key={item.id}
         style={{ backgroundImage: `url(${item.url})` }}
         to={`/film/${item.id}`}
       ></Link>)
@@ -73,7 +73,7 @@ class Slider extends Component {
         classer={index === this.state.current ? 'current-slide' : ''}
         slideNumber={index}
         certainSlide={this.certainSlide}
-        key={item.name}
+        key={item.id}
       />
     )
   }
