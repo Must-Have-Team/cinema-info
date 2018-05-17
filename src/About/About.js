1b949048d3295c94da8af6817e126b362f69769c
Bohdan Shuliaka <bohdan.shuliaka@gmail.com>
2018-05-17 10:54:48 +0300

    refactored

--- src/About/About.js
+++ src/About/About.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 import Footer from '../Footer'
 import './About.css';
 import Bohdan from './Photos/Bohdan.jpg';
-import Volodymyr from './Photos/Volodymyr.jpg'
-import Denys from './Photos/Denys.jpg'
-import Kateryna from './Photos/Kateryna.jpg'
+import Volodymyr from './Photos/Volodymyr.jpg';
+import Denys from './Photos/Denys.jpg';
+import Kateryna from './Photos/Kateryna.jpg';
+import Ihor from './Photos/Ihor.jpg';
 
 class About extends Component {
   render() {
@@ -13,115 +14,141 @@ class About extends Component {
       <div className="wrapper row2">
   <div id="container" className="clear">
 
- <section class="bg-light" id="team">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12 text-center">
-            <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
-            <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+ <section className="bg-light" id="team">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <h2 className="section-heading text-uppercase">Our Amazing Team</h2>
+            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
           </div>
         </div>
-        <div class="row">
-        <div class="col-sm-3">
-            <div class="team-member">
-              <img class="mx-auto rounded-circle" src={Volodymyr} alt="Volodymyr"/>
+        <div className="row">
+        <div className="col-md-2">
+            <div className="team-member">
+              <img className="mx-auto rounded-circle about" src={Volodymyr} alt="Volodymyr"/>
               <h4>Volodymyr Pasternak</h4>
-              <p class="text-muted">Future Lead Developer</p>
-              <ul class="list-inline social-buttons">
-                <li class="list-inline-item">
+              <p className="text-muted">Future Lead Developer</p>
+              <ul className="list-inline social-buttons">
+                <li className="list-inline-item">
                   <a href="#">
-                    <i class="fa fa-twitter"></i>
+                    <i className="fa fa-twitter"></i>
                   </a>
                 </li>
-                <li class="list-inline-item">
+                <li className="list-inline-item">
                   <a href="#">
-                    <i class="fa fa-facebook"></i>
+                    <i className="fa fa-facebook"></i>
                   </a>
                 </li>
-                <li class="list-inline-item">
+                <li className="list-inline-item">
                   <a href="#">
-                    <i class="fa fa-linkedin"></i>
+                    <i className="fa fa-linkedin"></i>
                   </a>
                 </li>
               </ul>
             </div>
           </div>
-          <div class="col-sm-3">
-            <div class="team-member">
-              <img class="mx-auto rounded-circle" src={Bohdan} alt="Bohdan"/>
+          <div className="col-md-2">
+            <div className="team-member">
+              <img className="mx-auto rounded-circle about" src={Bohdan} alt="Bohdan"/>
+              <div id="member-description">
               <h4>Bohdan Shuliaka</h4>
-              <p class="text-muted">Future Lead Developer</p>
-              <ul class="list-inline social-buttons">
-                <li class="list-inline-item">
+              <p className="text-muted">Future Lead Developer</p>
+              <ul className="list-inline social-buttons">
+                <li className="list-inline-item">
                   <a href="#">
-                    <i class="fa fa-twitter"></i>
+                    <i className="fa fa-twitter"></i>
                   </a>
                 </li>
-                <li class="list-inline-item">
+                <li className="list-inline-item">
                   <a href="#">
-                    <i class="fa fa-facebook"></i>
+                    <i className="fa fa-facebook"></i>
                   </a>
                 </li>
-                <li class="list-inline-item">
+                <li className="list-inline-item">
                   <a href="#">
-                    <i class="fa fa-linkedin"></i>
+                    <i className="fa fa-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-2">
+            <div className="team-member">
+              <img id="ihor" className="mx-auto rounded-circle about" src={Ihor} alt="Ігор"/>
+              <h4>Ihor Mykhaylyuk</h4>
+              <p className="text-muted">Mentor</p>
+              <ul className="list-inline social-buttons">
+                <li className="list-inline-item">
+                  <a href="#">
+                    <i className="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#">
+                    <i className="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a href="#">
+                    <i className="fa fa-linkedin"></i>
                   </a>
                 </li>
               </ul>
             </div>
           </div>
-          <div class="col-sm-3">
-            <div class="team-member">
-              <img class="mx-auto rounded-circle" src={Denys} alt="Denys"/>
+          <div className="col-md-2">
+            <div className="team-member">
+              <img className="mx-auto rounded-circle about" src={Denys} alt="Denys"/>
               <h4>Denys Palyarush</h4>
-              <p class="text-muted">Future Lead Developer</p>
-              <ul class="list-inline social-buttons">
-                <li class="list-inline-item">
+              <p className="text-muted">Future Lead Developer</p>
+              <ul className="list-inline social-buttons">
+                <li className="list-inline-item">
                   <a href="#">
-                    <i class="fa fa-twitter"></i>
+                    <i className="fa fa-twitter"></i>
                   </a>
                 </li>
-                <li class="list-inline-item">
+                <li className="list-inline-item">
                   <a href="#">
-                    <i class="fa fa-facebook"></i>
+                    <i className="fa fa-facebook"></i>
                   </a>
                 </li>
-                <li class="list-inline-item">
+                <li className="list-inline-item">
                   <a href="#">
-                    <i class="fa fa-linkedin"></i>
+                    <i className="fa fa-linkedin"></i>
                   </a>
                 </li>
               </ul>
             </div>
           </div>
-          <div class="col-sm-3">
-            <div class="team-member">
-              <img class="mx-auto rounded-circle" src={Kateryna} alt="Kateryna"/>
+          <div className="col-md-2">
+            <div className="team-member">
+              <img className="mx-auto rounded-circle about" src={Kateryna} alt="Kateryna"/>
               <h4>Kateryna Pynka</h4>
-              <p class="text-muted">Future Lead Developer</p>
-              <ul class="list-inline social-buttons">
-                <li class="list-inline-item">
+              <p className="text-muted">Future Lead Developer</p>
+              <ul className="list-inline social-buttons">
+                <li className="list-inline-item">
                   <a href="#">
-                    <i class="fa fa-twitter"></i>
+                    <i className="fa fa-twitter"></i>
                   </a>
                 </li>
-                <li class="list-inline-item">
+                <li className="list-inline-item">
                   <a href="#">
-                    <i class="fa fa-facebook"></i>
+                    <i className="fa fa-facebook"></i>
                   </a>
                 </li>
-                <li class="list-inline-item">
+                <li className="list-inline-item">
                   <a href="#">
-                    <i class="fa fa-linkedin"></i>
+                    <i className="fa fa-linkedin"></i>
                   </a>
                 </li>
               </ul>
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-lg-8 mx-auto text-center">
-            <p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+        <div className="row">
+          <div className="col-md-8 mx-auto text-center">
+            <p className="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
           </div>
         </div>
       </div>
